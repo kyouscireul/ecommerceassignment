03a9d0388f2fcfed89747cd5ff8541c39d819bc0
kyouscireul <sciostasis@gmail.com>
2025-06-27 22:40:47 +0800

    DATABASE PROGRESS

---
+++ data/productData.js
@@ -0,0 +1,63 @@
+export const clothes = [
+        { id: 1, name: 'Men Shirt 1', price: 100, images: ['images/IMG_3265.jpg', 'images/IMG_3265.jpg'], colors: ['#000000', '#E0FFFF', '	#8B4513'], sizes: ['M', 'L'], category: 'men' },
+        { id: 2, name: 'Men Shirt 2', price: 150, images: ['https://via.placeholder.com/300x400?text=Front', 'images/sweat shirt.jpg'], colors: ['#000000',  '	#8B4513', '	#FFF0F5'], sizes: ['S', 'M'], category: 'men' },
+        { id: 3, name: 'Men Shirt 3', price: 150, images: ['https://via.placeholder.com/300x400?text=Front', 'images/shirtblack.jpg'], colors: ['#000000', '#E0FFFF',  '	#FFF0F5'], sizes: ['S', 'M'], category: 'men' },
+        { id: 4, name: 'Men Shirt 4', price: 150, images: ['https://via.placeholder.com/300x400?text=Front', 'images/whiteshirt.png'], colors: ['#000000', '#E0FFFF', '	#8B4513', '	#FFF0F5'], sizes: ['S', 'M'], category: 'men' },
+         { id: 5, name: 'Men Shirt 5', price: 150, images: ['https://via.placeholder.com/300x400?text=Front', 'images/blazer.png'], colors: ['#000000', '#E0FFFF', '	#8B4513', '	#FFF0F5'], sizes: ['S', 'M'], category: 'men' },
+        
+         { id: 6, name: 'Women Dress 1', price: 200, images: ['https://via.placeholder.com/300x400?text=Front', 'images/floraldress.png'], colors: ['#000000', '#E0FFFF', '	#8B4513', '	#FFF0F5'], sizes: ['M', 'L', 'XL'], category: 'women' },
+         { id: 7, name: 'Women Dress 2', price: 200, images: ['https://via.placeholder.com/300x400?text=Front', 'images/greensweater.png'], colors: [ '#E0FFFF', '	#8B4513', '	#FFF0F5'], sizes: ['M', 'L', 'XL'], category: 'women' },
+         { id: 8, name: 'Women Dress 3', price: 200, images: ['https://via.placeholder.com/300x400?text=Front', 'images/greysweatshirt.png'], colors: ['#000000', '#E0FFFF', '	#8B4513', '	#FFF0F5'], sizes: ['M', 'L', 'XL'], category: 'women' },
+         { id: 9, name: 'Women Dress 4', price: 200, images: ['https://via.placeholder.com/300x400?text=Front', 'images/top.png'], colors: ['#000000',  '#8B4513', '	#FFF0F5'], sizes: ['M', 'L', 'XL'], category: 'women' },
+         { id: 10, name: 'Women Dress 5', price: 200, images: ['https://via.placeholder.com/300x400?text=Front', 'images/pajama.png'], colors: ['#000000', '#E0FFFF', '	#8B4513', '	#FFF0F5'], sizes: ['M', 'L', 'XL'], category: 'women' },
+        
+         { id: 11, name: 'Kids Tee 1', price: 80, images: ['https://via.placeholder.com/300x400?text=Front', 'images/dresspinkkid.png'], colors: ['#000000', '#E0FFFF', '	#FFF0F5'], sizes: ['S', 'M'], category: 'kids' },
+         { id: 12, name: 'Kids Tee 2', price: 80, images: ['https://via.placeholder.com/300x400?text=Front', 'images/bluedress.png'], colors: ['#000000', '#E0FFFF', '	#8B4513', 	], sizes: ['S', 'M'], category: 'kids' },
+         { id: 13, name: 'Kids Tee 3', price: 80, images: ['https://via.placeholder.com/300x400?text=Front', 'images/sweaterkids.png'], colors: ['#000000', '#E0FFFF', '	#8B4513', '	#FFF0F5'], sizes: ['S', 'M'], category: 'kids' },
+         { id: 14, name: 'Kids Tee 4', price: 80, images: ['https://via.placeholder.com/300x400?text=Front', 'images/whitedressk.png'], colors: ['#000000', '#E0FFFF', '	#8B4513', '	#FFF0F5'], sizes: ['S', 'M'], category: 'kids' },
+        { id: 15, name: 'Kids Tee 5', price: 80, images: ['https://via.placeholder.com/300x400?text=Front', 'images/setboy'], colors: ['#000000', '#E0FFFF', '	#8B4513'], sizes: ['S', 'M'], category: 'kids' },
+
+      ];
+
+export const appliances = [
+        { id: 1, name: 'Men Shirt 1', price: 100, images: ['images/IMG_3265.jpg', 'images/IMG_3265.jpg'], colors: ['#000000', '#E0FFFF', '	#8B4513'], sizes: ['M', 'L'], category: 'men' },
+        { id: 2, name: 'Men Shirt 2', price: 150, images: ['https://via.placeholder.com/300x400?text=Front', 'images/sweat shirt.jpg'], colors: ['#000000',  '	#8B4513', '	#FFF0F5'], sizes: ['S', 'M'], category: 'men' },
+        { id: 3, name: 'Men Shirt 3', price: 150, images: ['https://via.placeholder.com/300x400?text=Front', 'images/shirtblack.jpg'], colors: ['#000000', '#E0FFFF',  '	#FFF0F5'], sizes: ['S', 'M'], category: 'men' },
+        { id: 4, name: 'Men Shirt 4', price: 150, images: ['https://via.placeholder.com/300x400?text=Front', 'images/whiteshirt.png'], colors: ['#000000', '#E0FFFF', '	#8B4513', '	#FFF0F5'], sizes: ['S', 'M'], category: 'men' },
+         { id: 5, name: 'Men Shirt 5', price: 150, images: ['https://via.placeholder.com/300x400?text=Front', 'images/blazer.png'], colors: ['#000000', '#E0FFFF', '	#8B4513', '	#FFF0F5'], sizes: ['S', 'M'], category: 'men' },
+        
+         { id: 6, name: 'Women Dress 1', price: 200, images: ['https://via.placeholder.com/300x400?text=Front', 'images/floraldress.png'], colors: ['#000000', '#E0FFFF', '	#8B4513', '	#FFF0F5'], sizes: ['M', 'L', 'XL'], category: 'women' },
+         { id: 7, name: 'Women Dress 2', price: 200, images: ['https://via.placeholder.com/300x400?text=Front', 'images/greensweater.png'], colors: [ '#E0FFFF', '	#8B4513', '	#FFF0F5'], sizes: ['M', 'L', 'XL'], category: 'women' },
+         { id: 8, name: 'Women Dress 3', price: 200, images: ['https://via.placeholder.com/300x400?text=Front', 'images/greysweatshirt.png'], colors: ['#000000', '#E0FFFF', '	#8B4513', '	#FFF0F5'], sizes: ['M', 'L', 'XL'], category: 'women' },
+         { id: 9, name: 'Women Dress 4', price: 200, images: ['https://via.placeholder.com/300x400?text=Front', 'images/top.png'], colors: ['#000000',  '#8B4513', '	#FFF0F5'], sizes: ['M', 'L', 'XL'], category: 'women' },
+         { id: 10, name: 'Women Dress 5', price: 200, images: ['https://via.placeholder.com/300x400?text=Front', 'images/pajama.png'], colors: ['#000000', '#E0FFFF', '	#8B4513', '	#FFF0F5'], sizes: ['M', 'L', 'XL'], category: 'women' },
+        
+         { id: 11, name: 'Kids Tee 1', price: 80, images: ['https://via.placeholder.com/300x400?text=Front', 'images/dresspinkkid.png'], colors: ['#000000', '#E0FFFF', '	#FFF0F5'], sizes: ['S', 'M'], category: 'kids' },
+         { id: 12, name: 'Kids Tee 2', price: 80, images: ['https://via.placeholder.com/300x400?text=Front', 'images/bluedress.png'], colors: ['#000000', '#E0FFFF', '	#8B4513', 	], sizes: ['S', 'M'], category: 'kids' },
+         { id: 13, name: 'Kids Tee 3', price: 80, images: ['https://via.placeholder.com/300x400?text=Front', 'images/sweaterkids.png'], colors: ['#000000', '#E0FFFF', '	#8B4513', '	#FFF0F5'], sizes: ['S', 'M'], category: 'kids' },
+         { id: 14, name: 'Kids Tee 4', price: 80, images: ['https://via.placeholder.com/300x400?text=Front', 'images/whitedressk.png'], colors: ['#000000', '#E0FFFF', '	#8B4513', '	#FFF0F5'], sizes: ['S', 'M'], category: 'kids' },
+        { id: 15, name: 'Kids Tee 5', price: 80, images: ['https://via.placeholder.com/300x400?text=Front', 'images/setboy'], colors: ['#000000', '#E0FFFF', '	#8B4513'], sizes: ['S', 'M'], category: 'kids' },
+      
+      ];
+
+
+export const gadgets = [
+        { id: 1, name: 'Men Shirt 1', price: 100, images: ['images/IMG_3265.jpg', 'images/IMG_3265.jpg'], colors: ['#000000', '#E0FFFF', '	#8B4513'], sizes: ['M', 'L'], category: 'men' },
+        { id: 2, name: 'Men Shirt 2', price: 150, images: ['https://via.placeholder.com/300x400?text=Front', 'images/sweat shirt.jpg'], colors: ['#000000',  '	#8B4513', '	#FFF0F5'], sizes: ['S', 'M'], category: 'men' },
+        { id: 3, name: 'Men Shirt 3', price: 150, images: ['https://via.placeholder.com/300x400?text=Front', 'images/shirtblack.jpg'], colors: ['#000000', '#E0FFFF',  '	#FFF0F5'], sizes: ['S', 'M'], category: 'men' },
+        { id: 4, name: 'Men Shirt 4', price: 150, images: ['https://via.placeholder.com/300x400?text=Front', 'images/whiteshirt.png'], colors: ['#000000', '#E0FFFF', '	#8B4513', '	#FFF0F5'], sizes: ['S', 'M'], category: 'men' },
+         { id: 5, name: 'Men Shirt 5', price: 150, images: ['https://via.placeholder.com/300x400?text=Front', 'images/blazer.png'], colors: ['#000000', '#E0FFFF', '	#8B4513', '	#FFF0F5'], sizes: ['S', 'M'], category: 'men' },
+        
+         { id: 6, name: 'Women Dress 1', price: 200, images: ['https://via.placeholder.com/300x400?text=Front', 'images/floraldress.png'], colors: ['#000000', '#E0FFFF', '	#8B4513', '	#FFF0F5'], sizes: ['M', 'L', 'XL'], category: 'women' },
+         { id: 7, name: 'Women Dress 2', price: 200, images: ['https://via.placeholder.com/300x400?text=Front', 'images/greensweater.png'], colors: [ '#E0FFFF', '	#8B4513', '	#FFF0F5'], sizes: ['M', 'L', 'XL'], category: 'women' },
+         { id: 8, name: 'Women Dress 3', price: 200, images: ['https://via.placeholder.com/300x400?text=Front', 'images/greysweatshirt.png'], colors: ['#000000', '#E0FFFF', '	#8B4513', '	#FFF0F5'], sizes: ['M', 'L', 'XL'], category: 'women' },
+         { id: 9, name: 'Women Dress 4', price: 200, images: ['https://via.placeholder.com/300x400?text=Front', 'images/top.png'], colors: ['#000000',  '#8B4513', '	#FFF0F5'], sizes: ['M', 'L', 'XL'], category: 'women' },
+         { id: 10, name: 'Women Dress 5', price: 200, images: ['https://via.placeholder.com/300x400?text=Front', 'images/pajama.png'], colors: ['#000000', '#E0FFFF', '	#8B4513', '	#FFF0F5'], sizes: ['M', 'L', 'XL'], category: 'women' },
+        
+         { id: 11, name: 'Kids Tee 1', price: 80, images: ['https://via.placeholder.com/300x400?text=Front', 'images/dresspinkkid.png'], colors: ['#000000', '#E0FFFF', '	#FFF0F5'], sizes: ['S', 'M'], category: 'kids' },
+         { id: 12, name: 'Kids Tee 2', price: 80, images: ['https://via.placeholder.com/300x400?text=Front', 'images/bluedress.png'], colors: ['#000000', '#E0FFFF', '	#8B4513', 	], sizes: ['S', 'M'], category: 'kids' },
+         { id: 13, name: 'Kids Tee 3', price: 80, images: ['https://via.placeholder.com/300x400?text=Front', 'images/sweaterkids.png'], colors: ['#000000', '#E0FFFF', '	#8B4513', '	#FFF0F5'], sizes: ['S', 'M'], category: 'kids' },
+         { id: 14, name: 'Kids Tee 4', price: 80, images: ['https://via.placeholder.com/300x400?text=Front', 'images/whitedressk.png'], colors: ['#000000', '#E0FFFF', '	#8B4513', '	#FFF0F5'], sizes: ['S', 'M'], category: 'kids' },
+        { id: 15, name: 'Kids Tee 5', price: 80, images: ['https://via.placeholder.com/300x400?text=Front', 'images/setboy'], colors: ['#000000', '#E0FFFF', '	#8B4513'], sizes: ['S', 'M'], category: 'kids' },
+      
+      ];
